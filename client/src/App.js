485a58aa6c4fe753ff057e26066cdd883a52b83d
wemersonsantos <wemersonsantoseng@gmail.com>
2021-08-30 21:43:37 -0300

    Instalando o react.js e testando o funcionamento do react-burger-menu

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+//import Sidebar from './pages/Sidebar/Sidebar'
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App" id='outer-container'>
+      <div id="page-wrap">
+        {/*<Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'}/>*/}
+        <h1>Cool Restaurant</h1>
+        <h2>Check out our offerings in the sidebar!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
